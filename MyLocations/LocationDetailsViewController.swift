530e8d332e375fbd5902449dacc2739828cc96b3
vysotskiy1998 <vysotskiy.boss@gmail.com>
2023-01-08 13:23:37 +0200

    Added new LocationDetailsViewController

---
+++ MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsDetailViewController.swift
+//  MyLocations
+//
+//  Created by Vysotskiy Vladimir on 08.01.2023.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
