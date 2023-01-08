c1be74d98a81e1fcbacb4ef4e59efcb6ae564d52
vysotskiy1998 <vysotskiy.boss@gmail.com>
2023-01-08 15:18:36 +0200

    Finished layout for LocationDetailsViewController.swift

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 class LocationDetailsViewController: UITableViewController {
     
     
+    
+    
+    
+    @IBAction func Done(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    @IBAction func Cancel(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
